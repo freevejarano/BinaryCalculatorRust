1bd87ae2ecab7d7eac7086371ceb8d8804cd9669
alejandro <luis.vejarano01@correo.usa.edu.co>
2022-03-28 20:11:55 -0500

    Suma

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+use std::cmp;
+
 
 fn input(msg: &str) -> i32 {
     println!("{}", msg);
@@ -32,9 +34,47 @@ fn to_binary(mut decimal: i32) -> Vec<i32>{
     bin
 }
 
+fn sum(mut a: Vec<i32>, mut b: Vec<i32>)->Vec<i32>{
+    let max_len = cmp::max(a.len(), b.len());
+
+    while a.len() != max_len || b.len()!=max_len{
+        if a.len() != max_len {
+            a.reverse();
+            a.push(0);
+            a.reverse();
+        }else if b.len() != max_len {
+            b.reverse();
+            b.push(0);
+            b.reverse();
+        }
+
+    }
+
+    let mut result = Vec::new();
+    let mut carry = 0;
+    let mut i = max_len + 1;
+
+    while i > 1{
+        let mut r = carry;
+        r = r + a[i-2];
+        r = r + b[i-2];
+        result.push(if (r % 2 == 1) {1} else{0});
+        carry = if r < 2 {0} else{1};
+        i -= 1;
+    }
+    if carry != 0 {
+        result.push(1);
+    }
+    result.reverse();
+    println!("{:?}", result);
+    result
+}
+
 fn main() {
-    let number = input("Enter an input");
-    println!("{:?}", to_binary(number));
+    let number = input("Ingresa un numero: ");
+    let number2 = input("Ingresa un numero: ");
+    //println!("{:?}", to_binary(number));
+    sum(to_binary(number), to_binary(number2));
 }
 
 
